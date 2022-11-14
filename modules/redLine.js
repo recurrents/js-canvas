311170e0c4762c783900f7e81464380441467d86
Johan Melin <johan.melin@picsmart.se>
2022-11-14 08:47:04 +0100

    added new init funcs

--- modules/redLine.js
+++ modules/redLine.js
@@ -1,24 +1,41 @@
 const pos = [];
 const amount = 720; // canv.width
 const color = "rgba(255, 64, 64, 0.02)";
+let canv;
 const dot = {
   size: 1,
   speed: 0.02,
-  x: 720 / 2,
-  y: 480 / 2,
+  x: 0,
+  y: 0,
   xv: 0,
   yv: 0,
+
+  center(canv) {
+    this.x = canv.width / 2;
+    this.y = canv.height / 2;
+  },
+
+  randomPos(canv) {
+    this.x = Math.random() * canv.width;
+    this.y = Math.random() * canv.height;
+  },
+  
+  line(canv, i) {
+    this.x = i;
+    this.y = canv.height / 2;
+  },
 
   step() {
     return Math.random() * this.speed - (this.speed / 2);
   },
 
-  move() {
+  move(ctx) {
     this.xv += this.step();
     this.yv += this.step();
     this.x += this.xv;
     this.y += this.yv;
     this.bounce();
+    ctx.fillRect(this.x, this.y, this.size, this.size);
   },
 
   wrap() {
@@ -34,16 +51,19 @@ const dot = {
   },
 }
 
-for(let i = 0; i < amount; i++) {
-  pos.push(Object.create(dot));
-}
-
 export function redLine(canv, ctx){
   ctx.fillStyle = color;
+
+  for(let i = 0; i < amount; i++) {
+    const newDot = Object.create(dot);
+    //newDot.randomPos(canv);
+    newDot.line(canv, i)
+    pos.push(newDot);
+  }
+
   function draw() {
     for(let obj of pos) {
-      obj.move();
-      ctx.fillRect(obj.x, obj.y, dot.size, dot.size);
+      obj.move(ctx);
     }
     requestAnimationFrame(draw);
   }
